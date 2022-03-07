06c58341e98e8970072422db6410a532f8100289
willreis <william.reis.silva@gmail.com>
2022-03-07 17:38:30 -0300

    get feito

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React, { useState, useEffect } from "react";
 import api from "./services/Api";
 
 export default function App() {
+  const [pokemon, setPokemon] = useState([])
   const [poke, setPoke] = useState();
+  const [img, setImg] = useState();
+  const [type, setType] = useState();
+  const[name, setNome] = useState();
 
   useEffect(() => {
     api
-      .get("/pokemon")
-      .then((resposta) => {
-        console.log(resposta);
-        setPoke(resposta.data);
+      .get("/pokemon/charmeleon")
+      .then((response) => {
+        console.log(response);
+        console.log('nome :', response.data.name)
+        setPoke(response.data);
+        console.log('asdfasdfa', poke.name)
+        console.log('tipo: ', poke.types[0].type.name)
       })
       .catch((error) => {
         console.log("Ocorreu um erro", error);
@@ -20,14 +27,9 @@ export default function App() {
     <>
       <h2>Pokédex</h2>
       <div>
-        {poke.map((pokes) => (
-          <div>
-            <h3 key={pokes.id}>{pokes.name}</h3>
-            <div>{pokes.sprites}</div>
-          </div>
-        ))}
-        <h3>nome poke</h3>
-        <div>foto poke</div>
+        <h3>nome poke: {poke.name}</h3> 
+        <div><img src={poke.sprites.front_default} alt="" /></div>
+        <h3>tipo: {poke.types[0].type.name}</h3>
       </div>
     </>
   );
